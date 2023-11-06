b7aaf53a89547a74c248435dcb36c5ca22ac831f
JORDI GALLEN RENAU <>
2023-11-06 18:00:21 +0100

    Preparation of the Screen tests

---
+++ RandomUserPackage/Tests/PresentationTest/Modules/Detail/View/UserDetailAssemblyTest.swift
@@ -0,0 +1,23 @@
+//
+//  UserDetailAssemblyTest.swift
+//
+//
+//  Created by JORDI GALLEN RENAU on 6/11/23.
+//
+
+@testable import Domain
+@testable import Presentation
+import XCTest
+
+final class UserDetailAssemblyTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func test_get_userDetail_View_when_build() {
+//        let viewController = sut.build()
+//
+//        XCTAssertTrue(viewController is UsersView)
+    }
+}
---
+++ RandomUserPackage/Tests/PresentationTest/Modules/List/View/UsersListAssemblyTest.swift
@@ -0,0 +1,24 @@
+//
+//  UsersListAssemblyTest.swift
+//
+//
+//  Created by JORDI GALLEN RENAU on 6/11/23.
+//
+
+@testable import Domain
+@testable import Presentation
+import XCTest
+
+final class UsersListAssemblyTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func test_get_usersView_when_build() {
+//        let viewController = sut.build()
+//        
+//        XCTAssertTrue(viewController is UsersView)
+    }
+    
+}
